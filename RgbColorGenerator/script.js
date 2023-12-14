120384879a4aa04f84649aeb867080538f2bebbe
unknown <pablo.peralta94@gmail.com>
2023-12-14 20:02:33 -0300

    Tip Calculator

--- RgbColorGenerator/script.js
+++ RgbColorGenerator/script.js
@@ -1,6 +1,6 @@
-const redSlider = document.getElementById("redSlider");
-const greenSlider = document.getElementById("greenSlider");
-const blueSlider = document.getElementById("blueSlider");
+const redSlider = document.getElementById("redslider");
+const greenSlider = document.getElementById("greenslider");
+const blueSlider = document.getElementById("blueslider");
 
 const redValueSpan = document.getElementById("redValue");
 const greenValueSpan = document.getElementById("greenValue");
@@ -10,51 +10,39 @@ const colorBox = document.getElementById("color-box");
 const copyButton = document.getElementById("copyButton");
 const inputTypeRGBValue = document.getElementById("inputType");
 
-redSlider.addEventListener('input',updateColor);
-greenSlider.addEventListener('input',updateColor);
-blueSlider.addEventListener('input',updateColor);
-copyButton.addEventListener('input',copyRGBValue);
-
+redSlider.addEventListener('input', updateColor);
+greenSlider.addEventListener('input', updateColor);
+blueSlider.addEventListener('input', updateColor);
+copyButton.addEventListener('click', copyRGBValue);
 
 function updateColor() {
-
     const redValue = redSlider.value;
     const greenValue = greenSlider.value;
     const blueValue = blueSlider.value;
 
     const rgbColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-
 
     colorBox.style.backgroundColor = rgbColor;
 
     redValueSpan.textContent = redValue;
     greenValueSpan.textContent = greenValue;
     blueValueSpan.textContent = blueValue;
-
-
-
 }
 
-
-updateColor()
-
-
-
+updateColor();
 
 function copyRGBValue() {
-
     const redValue = redSlider.value;
     const greenValue = greenSlider.value;
     const blueValue = blueSlider.value;
 
     const rgbColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 
-    navigator.clickboard.writeText(rgbColor)
-    .then(()=>{
-        alert("RGB COLOR Value copied to Clipboard: " + rgbColor);
-
-    })
-    .catch((error)=> {
-        console.error("failed to copy RGB values", error);
-    })
+    navigator.clipboard.writeText(rgbColor)
+        .then(() => {
+            alert("RGB COLOR Value copied to Clipboard: " + rgbColor);
+        })
+        .catch((error) => {
+            console.error("failed to copy RGB values", error);
+        });
 }
